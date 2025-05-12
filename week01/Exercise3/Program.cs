ca21348ca8233b6123ce02f300d50958ca8cfd6c
McWise02 <micah02.brown@gmail.com>
2025-05-12 18:20:36 +0200

    assignments

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); 
+
+        int guess = -1; 
+
+        Console.WriteLine("Guess My Number Game!");
+
+        
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
